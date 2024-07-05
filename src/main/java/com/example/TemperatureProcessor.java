7ea1ae9f3b18ef58b9a0a6f0e411f21f692bbac6
Woxet <seillierjordan1@gmail.com>
2024-07-05 17:18:38 +0200

    Change

--- src/main/java/com/example/TemperatureProcessor.java
+++ src/main/java/com/example/TemperatureProcessor.java
@@ -1,51 +1,35 @@
 package com.example;
 
-import io.quarkus.runtime.StartupEvent;
-import javax.inject.Inject;
-
-import io.smallrye.reactive.messaging.annotations.Blocking;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
+import java.time.Instant;
 import java.util.Properties;
 
-@ApplicationScoped
 public class TemperatureProcessor {
+    public final String CELSIUS_TOPIC = "celsius-topic";
+    public final String FAHRENHEIT_TOPIC = "fahrenheit-topic";
 
-    StreamsBuilder streamsBuilder;
-
-    public void onStart(@Observes StartupEvent event) {
-        createTopology();
-    }
-
-    private void createTopology() {
-        KStream<String, Integer> sourceStream = streamsBuilder.stream("temperature-celsius-topic");
-
-        KStream<String, Integer> convertedStream = sourceStream.mapValues(celsius -> celsiusToFahrenheit(celsius));
-
-        convertedStream.to("temperature-fahrenheit-topic", Produced.with(Serdes.String(), Serdes.Integer()));
-
-        Topology topology = streamsBuilder.build();
-        KafkaStreams kafkaStreams = new KafkaStreams(topology, getProperties());
-        kafkaStreams.start();
-    }
-
-    private int celsiusToFahrenheit(int celsius) {
-        return (int) (celsius * 9.0 / 5.0 + 32);
-    }
-
-    private Properties getProperties() {
+    public void process(){
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "temperature-conversion");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
-        return props;
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-temperature-converter");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, Double> kStream = builder.stream(CELSIUS_TOPIC, Consumed.with(Serdes.String(), Serdes.Double()))
+                .map((key, value) -> new KeyValue<>(key, (value * 9/5) + 32))
+                .peek((k, v) -> System.out.println("Key = " + k + " Value = " + v))
+                ;
+
+        kStream.to(FAHRENHEIT_TOPIC);
+
+        Topology topology = builder.build();
+
+        KafkaStreams streams = new KafkaStreams(topology, props);
+        streams.start();
     }
+
 }
+
